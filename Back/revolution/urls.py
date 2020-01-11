36962613e7083671c19d25337ee96e6ddcfa2e88
tyrrr-aj <adam.szreter@gmail.com>
2020-01-11 18:16:34 +0100

    endpoint attepmt v2

--- Back/revolution/urls.py
+++ Back/revolution/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from revolution import views
 
 urlpatterns = [
-    path('problem/<int:pk>', views.problemDetails.as_view())
+    path('problem/<int:pk>', views.ProblemDetails.as_view()),
+    path('problem/new', views.NewProblem.as_view()),
 ]
--- Back/revolution/views.py
+++ Back/revolution/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from revolution.models import Problem
+from revolution.serializers import ProblemSerializer
+from rest_framework import generics
 
-# Create your views here.
+
+class ProblemDetails(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Problem.objects.all()
+    serializer_class = ProblemSerializer
+
+
+class NewProblem(generics.CreateAPIView):
+    queryset = Problem.objects.all()
+    serializer_class = ProblemSerializer
