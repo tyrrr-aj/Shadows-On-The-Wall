363acadeae136339fb2a32cd02465a060d3cb61c
WookashWackomy <wookie.xp.07@gmail.com>
2020-01-12 02:54:05 +0100

    xd

---
+++ Front/frontend-app/src/components/SubmissionGraphPage/SubmissionGraphPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const SubmissionGraphPage = props => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+SubmissionGraphPage.propTypes = {
+
+}
+
+export default SubmissionGraphPage
